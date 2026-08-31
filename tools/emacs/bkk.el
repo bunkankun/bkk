e10d5147b8395c8883a2157b3201d6b38f22e289
ChristianW <cwittern@yahoo.com>
2026-08-31 15:58:05 +0900

    Added stub for bkk.el

---
+++ tools/emacs/bkk.el
@@ -0,0 +1,25 @@
+;;; bkk.el --- A supporting mode for bunkankun.org -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2026 Christian Wittern
+;;
+;; Author: Christian Wittern <cwittern@yahoo.com>
+;; Maintainer: Christian Wittern <cwittern@yahoo.com>
+;; Created: August 31, 2026
+;; Modified: August 31, 2026
+;; Version: 0.0.1
+;; Keywords: Premodern Chinese, Translation, Interface
+;; Homepage: https://github.com/bunkankun/bkk
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'bkk)
+;;; bkk.el ends here
